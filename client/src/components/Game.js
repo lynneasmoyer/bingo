c2372be8c538af750a8581dacda77efa26a3ec31
Lynnea Smoyer <lynneasmoyer@gmail.com>
2020-03-30 12:27:14 -0500

    Added Confetti and Game components

---
+++ client/src/components/Game.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+class Game extends React.Component {
+
+
+render() {
+    return (
+
+<div>
+
+<table role='grid'>
+    <tbody>
+    <tr role='row'>
+        <td role='gridcell'>...</td>
+        <td role='gridcell'>...</td>
+        <td role='gridcell'>...</td>
+        <td role='gridcell'>...</td>
+        <td role='gridcell'>...</td>   
+    </tr>
+    <tr role='row'>
+        <td role='gridcell'>...</td>
+        <td role='gridcell'>...</td>
+        <td role='gridcell'>...</td>
+        <td role='gridcell'>...</td>
+        <td role='gridcell'>...</td>   
+    </tr>
+    <tr role='row'>
+        <td role='gridcell'>...</td>
+        <td role='gridcell'>...</td>
+        <td role='gridcell'>...</td>
+        <td role='gridcell'>...</td>
+        <td role='gridcell'>...</td>   
+    </tr>
+    <tr role='row'>
+        <td role='gridcell'>...</td>
+        <td role='gridcell'>...</td>
+        <td role='gridcell'>...</td>
+        <td role='gridcell'>...</td>
+        <td role='gridcell'>...</td>   
+    </tr>
+    <tr role='row'>
+        <td role='gridcell'>...</td>
+        <td role='gridcell'>...</td>
+        <td role='gridcell'>...</td>
+        <td role='gridcell'>...</td>
+        <td role='gridcell'>...</td>   
+    </tr>
+    
+    </tbody>
+</table>
+
+</div>
+    );
+    }
+}
+
+export default Game;
---
+++ client/src/views/Main.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
+import { Link } from '@reach/router';
+import Game from '../components/Game';
+
+const Main = (props) => {
+    
+    
+    
+    
+    return(
+        <div>
+        <Game></Game>
+    
+        </div>
+    );
+}
+
+
+export default Main;
