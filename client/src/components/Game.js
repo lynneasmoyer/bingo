62823e4adc17ffc41e66a8412b5da9f4603642c7
Lynnea Smoyer <lynneasmoyer@gmail.com>
2020-04-02 20:59:59 -0500

    Added Images component

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,59 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import Images from "./Images.js";
 
 class Game extends React.Component {
-
+    
 
 render() {
+    Images.map((id, src, title, description) => {
     return (
+        <div class= "gameboard">
+            <h1>Bingo</h1>
+            
+                <div>
+                    <table role='grid'>
+                        <tbody>
+                            <tr role='row'>
+                                <td role='gridcell'> <img key={id} src={src} title={title} alt={description}) /> </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            
+        </div>
+        
+    }  
 
-<div>
-
-<table role='grid'>
-    <tbody>
-    <tr role='row'>
-        <td role='gridcell'>...</td>
-        <td role='gridcell'>...</td>
-        <td role='gridcell'>...</td>
-        <td role='gridcell'>...</td>
-        <td role='gridcell'>...</td>   
-    </tr>
-    <tr role='row'>
-        <td role='gridcell'>...</td>
-        <td role='gridcell'>...</td>
-        <td role='gridcell'>...</td>
-        <td role='gridcell'>...</td>
-        <td role='gridcell'>...</td>   
-    </tr>
-    <tr role='row'>
-        <td role='gridcell'>...</td>
-        <td role='gridcell'>...</td>
-        <td role='gridcell'>...</td>
-        <td role='gridcell'>...</td>
-        <td role='gridcell'>...</td>   
-    </tr>
-    <tr role='row'>
-        <td role='gridcell'>...</td>
-        <td role='gridcell'>...</td>
-        <td role='gridcell'>...</td>
-        <td role='gridcell'>...</td>
-        <td role='gridcell'>...</td>   
-    </tr>
-    <tr role='row'>
-        <td role='gridcell'>...</td>
-        <td role='gridcell'>...</td>
-        <td role='gridcell'>...</td>
-        <td role='gridcell'>...</td>
-        <td role='gridcell'>...</td>   
-    </tr>
-    
-    </tbody>
-</table>
-
-</div>
-    );
-    }
 }
+
+    }
+};
 
 export default Game;
---
+++ client/src/components/Images.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+const Images = [
+    {id: 1, src: './images/akita.jpg', description: "akita"},
+    {id: 2, src: './images/beagle.jpg', description: "beagle"}
+];
+
+export default Images;
